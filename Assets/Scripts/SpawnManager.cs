4fa5aa779da5b54a281cfed27f8e7f0207cb1d86
peco-tanaka <ok.computeruk1929@gmail.com>
2025-12-14 01:43:28 +0900

    refactor: 変数かしてリファクタ

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,7 +16,10 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
